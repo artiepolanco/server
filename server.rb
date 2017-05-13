fddae6e643593899835a622d9c187484ff5d0476
Arturo Polanco <artiepolanco@gmail.com>
2017-05-12 23:51:02 -0400

    Updated Sinatra to run ruby embedded file for jobs finder

--- server.rb
+++ server.rb
@@ -1,5 +1,18 @@
 require "sinatra"
+require "httparty"
+require "nokogiri"
+
+# get '/' do
+#   send_file File.join(settings.public_folder, 'job_finder.rb')
+# end
+
+# get '/' do
+#   load "/Users/arturopolanco/Desktop/wyncode/web-basic/job_finder.rb"
+# end
 
 get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+  url = "https://miami.craigslist.org/search/sof"
+  response = HTTParty.get url
+  @dom = Nokogiri::HTML.parse(response.body)
+  erb :job
 end
